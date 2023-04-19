e8b709feb44ea15ac203ca8ee040d0b22f34072e
Iankimble <ian.kimble571@gmail.com>
2023-04-19 09:13:27 -0400

    Python class and object practice

---
+++ python_class.py
@@ -0,0 +1,69 @@
+# How would you print out the names of the pets 
+
+my_pet_1= 'pet'
+my_pet_1_type = 'cat'
+my_pet_1_noise = 'meow'
+my_pet_1_full_name = 'Snuffles McGruff'
+ 
+my_pet_2 = 'pet'
+my_pet_2_type = 'cat'
+my_pet_2_noise = 'meow'
+my_pet_2_full_name = 'Snowpounce Flury' 
+
+# dictionaries
+my_pet_1= {
+'name':'Snuffles McGruff'
+}
+
+my_pet_2= {
+'name':'Snowpounce Flury'
+}
+
+# print(my_pet_1['name'])
+# print(my_pet_2['name'])
+
+pets=[
+{
+'name':'Snowpounce Flury'
+},
+{
+'name':'Snuffles McGruff'
+}
+]
+
+# for pet in pets:
+#     print(pet['name'])
+
+# solve for bugprint(my_pet_1("name"))
+
+# print(my_pet_1_full_name, my_pet_2_full_name)
+
+
+ 
+class Smoothies:
+    def __init__(self,ingredientA, ingredientB, ingredientC):
+        self.ingredientA= ingredientA
+        self.ingredientB= ingredientB
+        self.ingredientC= ingredientC
+
+order1= Smoothies('mango','blueberry','water')
+
+print(order1.ingredientA)
+print(order1.ingredientB)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
